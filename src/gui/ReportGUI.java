7ebb7b36ceffd84b366462a9fe5c3669785a81d5
jnbui94 <jnbui94@uw.edu>
2016-11-29 11:20:50 -0800

    added Report tab and reportGUI class

---
+++ src/gui/ReportGUI.java
@@ -0,0 +1,20 @@
+package gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class ReportGUI extends JPanel {
+
+    public ReportGUI(){
+        setLayout(new BorderLayout());
+        createComponents();
+        setVisible(true);
+        setSize(500, 500);
+    }
+
+    private void createComponents() {
+        // TODO Auto-generated method stub
+        
+    }
+}
