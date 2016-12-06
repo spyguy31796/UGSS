169b08db256fb33f5d7e06b783b7c65890a3a7a1
spyguy31796 <planetalec@live.com>
2016-12-06 08:14:01 -0800

    Some More Cleaning

--- src/data/DataConnection.java
+++ src/data/DataConnection.java
@@ -8,51 +8,64 @@ import java.util.Properties;
 
 /**
  * This class has the information to connect to the cssgate server. 
- * TODO - Update this file with your myuwnetid and mysqlpassword
  * to customize for your own project.
- * @author mabraham
+ * @author mabraham/GROUP8
+ * @version 12/6/2016
  *
  */
+public final class DataConnection {
 
-public class DataConnection {
-	
-	private static String userName = "_360team8"; //TODO - your uwnetid here
-	private static String password = "dasGiwum"; //TODO - your mysqlpassword here
-	private static String serverName = "cssgate.insttech.washington.edu";
-	private static Connection sConnection;
-	
-	// Creates once instance of the connection to be reused in the different places in the
-	// system. 
-	private static void createConnection() throws SQLException {
-		Properties connectionProps = new Properties();
-		connectionProps.put("user", userName);
-		connectionProps.put("password", password);
-		sConnection =  DriverManager
-				.getConnection("jdbc:mysql://" + serverName + "/" + userName + "?user=" + userName + "&password=" + password);
-				
-		//For debugging -*/ System.out.println("Connected to database");
-	}
-	
-	/**
-	 * Returns a connection to the database so that queries can be executed.
-	 * @return Connection to the database
-	 * @throws SQLException
-	 */
-	public static Connection getConnection() throws SQLException {
-		if (sConnection == null) {
-			createConnection();
-		}
-		return sConnection;
-	}
-	
-	/**
-	 * Close the connection to the database when done. 
-	 * @throws SQLException
-	 */
-	public static void closeConnection() throws SQLException {
-		if (sConnection != null && !sConnection.isClosed()) {
-			sConnection.close();
-		}
-	}
+    /**
+     * The unetid for SQL database login.
+     */
+    private static String userName = "_360team8"; 
+    /**
+    * The mysqlpassword for SQL database login.
+    */
+    private static String password = "dasGiwum";
+    /**
+     * Server address for SQL database.
+     */
+    private static String serverName = "cssgate.insttech.washington.edu";
+    /**
+    * Connection object.
+    */
+    private static Connection sConnection;
+    /**
+     * Satisfies Checkstyle.
+     */
+    private DataConnection() { }
+    /**Creates once instance of the connection to be reused in the different places in the
+     * system. 
+     * @throws SQLException SQL Exception.
+     */
+    private static void createConnection() throws SQLException {
+        final Properties connectionProps = new Properties();
+        connectionProps.put("user", userName);
+        connectionProps.put("password", password);
+        sConnection =  DriverManager.
+                getConnection("jdbc:mysql://" + serverName + "/" + userName + "?user="
+                                + userName + "&password=" + password);
+    }
+    /**
+    * Returns a connection to the database so that queries can be executed.
+    * @return Connection to the database
+    * @throws SQLException SQL Exception.
+    */
+    public static Connection getConnection() throws SQLException {
+        if (sConnection == null) {
+            createConnection();
+        }
+        return sConnection;
+    }
+    /**
+    * Close the connection to the database when done. 
+    * @throws SQLException SQL Exception.
+    */
+    public static void closeConnection() throws SQLException {
+        if (sConnection != null && !sConnection.isClosed()) {
+            sConnection.close();
+        }
+    }
 
 }
